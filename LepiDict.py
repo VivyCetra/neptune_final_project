5b85ac834166552b8962f6be9469eff43fee9f5a
Viviana Cetrangolo <vcetra87@gmail.com>
2016-06-09 16:40:15 +0200

    Added Function

--- LepiDict.py
+++ LepiDict.py
@@ -4,14 +4,18 @@ InFileName = "MM_Lepido_3_play.sbd"
 
 InFile = open(InFileName, 'r')
 
+
+counter=0
 for Line in InFile:
+	Mylist = []
 	Line = Line.split ()
 	if Line[0] == '---':
 		continue
 	if len(Line) == 1:  
-		print Line
+		for i in range (int(Line[0]))
+			mylist.append( [ Line [2],Line  [3] ] )
 
 
-
+ 
 
 
