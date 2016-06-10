a66ba4b4bf5cf938a161cc78bd33a171e92dca12
Viviana Cetrangolo <vcetra87@gmail.com>
2016-06-10 12:09:48 +0200

    Final script

--- LepiDict.py
+++ LepiDict.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 from collections import defaultdict 
+from scipy.spatial.distance import pdist
+import numpy
+
 InFileName = "Lepi_Cell.txt"
 
 InFile = open(InFileName, 'r')
@@ -11,31 +14,22 @@ for Line in InFile:
 	if len(Line.split())==1:
 		holder=str(counter)
 		print (holder)
-		print ('here'+Line)
+		print ('cell_number'+Line)
 		counter+=1
+
+ 
 	else:
 		mydict['cell-'+holder].append([Line.split()[1],Line.split()[2]])
-		#print (Line)
+		
 
-print (mydict['cell-0'])
-
-def distSquared(p0, p1):
-   return (p0[0][0] - p1[0][0])**2 + (p0[1][1] - p1[1][1])**2
- 
-oInput = [[9.5, 7.5], [0.2, 19.1]]
-print (distSquared(oInput,oInput))
 
 for keys in mydict.keys():
-	for pair in mydict[keys]:
-		print (distSquared(pair)) #one give one pair
+	if len(mydict[keys]) == 1:
+		continue
+	result = numpy.array(mydict[keys])
+	mindist = numpy.min(pdist(result))
+	maxdist = numpy.max(pdist(result))
 
+	print ('{}: min:{} max:{} ' .format(keys, mindist, maxdist))
 
-#def distance(points):
-#    p0, p1 = points
- #   return math.sqrt((P0[1] - p1[1])**2 + (p0[2] - p1[2])**2)
-
-  #	min_distance = distance(Line.split()[1],Line.split () [2])
-
-#	for p0, p1 in itertools.combinations(Line.split, 2):
- #   min_distance = min(min_distance, distance(p0, p1))
 
